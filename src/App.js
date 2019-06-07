b4e11a0b439a5a881dd0f67293dbc3fa47679de7
manny <manny@legworkprm.com>
2019-06-07 07:41:29 -0700

    Refactor: move state up for breeds list and convert Home to a functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Breed from './components/Breed';
+import * as Constants from './constants';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/breed/:name' component={Breed} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      breeds: [],
+    };
+  }
+
+  componentDidMount() {
+    // fetch list of dog breeds
+    fetch(`${Constants.DOG_API_URL}/breeds/list/all`)
+    .then(raw => raw.json())
+    .then(breeds => this.setState({breeds: this.mapBreeds(breeds.message)}))
+    .catch(err => {
+      // TODO: surface error to UI
+      console.log(err);
+    });
+  }
+
+  mapBreeds = breeds => {
+    // breeds should be an array of objects in the shape: 
+    // { breedId: <breed>[-<subbreed>], breedName: [<subbreed> ]<breed> }
+    let mappedBreeds = [];
+
+    Object.keys(breeds).reduce((acc, cur) => {
+      const breed = breeds[cur];
+      const hasSubBreeds = breed.length > 0;
+
+      if (hasSubBreeds) {
+        breed.forEach(sb => {
+          mappedBreeds.push({
+            breedId: `${cur}-${sb}`,
+            breedName: `${sb} ${cur}`
+          });
+        })
+      } else {
+        mappedBreeds.push({
+          breedId: cur,
+          breedName: cur,
+        });
+      }
+      
+      return acc;
+    }, mappedBreeds);
+
+    return mappedBreeds;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' render={props => <Home {...props} breeds={this.state.breeds}/>} />
+            <Route path='/breed/:name' component={Breed} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,90 +11,38 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Divider from '@material-ui/core/Divider';
 import Icon from '@material-ui/core/Icon';
 
-import * as Constants from '../constants';
+const Home = ({breeds}) => {
+  return (
+    <div className="home">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6">Breed List</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container
+        className="breed-list-container" maxWidth="xs">
+        <List>
+          {breeds.map((breed, index) => {
+            const { breedId, breedName } = breed;
+            return (
+              <div key={index}>
+                <ListItem
+                  button
+                  component={Link}
+                  to={`/breed/${breedId}`}>
+                  <ListItemIcon>
+                    <Icon>pets</Icon>
+                  </ListItemIcon>
+                  <ListItemText primary={breedName}/>
+                </ListItem>
+                <Divider/>
+              </div>
+            );
+          })}
+        </List>
+      </Container>
+    </div>
+  )
+};
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      breeds: [],
-    };
-  }
-
-  componentDidMount() {
-    // fetch list of dog breeds
-    fetch(`${Constants.DOG_API_URL}/breeds/list/all`)
-    .then(raw => raw.json())
-    .then(breeds => this.setState({breeds: this.mapBreeds(breeds.message)}))
-    .catch(err => {
-      // TODO: surface error to UI
-      console.log(err);
-    });
-  }
-
-  mapBreeds = breeds => {
-    // breeds should be an array of objects in the shape: 
-    // { breedId: <breed>[-<subbreed>], breedName: [<subbreed> ]<breed> }
-    let mappedBreeds = [];
-
-    Object.keys(breeds).reduce((acc, cur) => {
-      const breed = breeds[cur];
-      const hasSubBreeds = breed.length > 0;
-
-      if (hasSubBreeds) {
-        breed.forEach(sb => {
-          mappedBreeds.push({
-            breedId: `${cur}-${sb}`,
-            breedName: `${sb} ${cur}`
-          });
-        })
-      } else {
-        mappedBreeds.push({
-          breedId: cur,
-          breedName: cur,
-        });
-      }
-      
-      return acc;
-    }, mappedBreeds);
-
-    return mappedBreeds;
-  }
-
-  render() {
-    const { breeds } = this.state;
-
-    return (
-      <div className="home">
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography variant="h6">Breed List</Typography>
-          </Toolbar>
-        </AppBar>
-        <Container
-          className="breed-list-container" maxWidth="xs">
-          <List>
-            {breeds.map((breed, index) => {
-              const { breedId, breedName } = breed;
-              return (
-                <div key={index}>
-                  <ListItem
-                    button
-                    component={Link}
-                    to={`/breed/${breedId}`}>
-                    <ListItemIcon>
-                      <Icon>pets</Icon>
-                    </ListItemIcon>
-                    <ListItemText primary={breedName}/>
-                  </ListItem>
-                  <Divider/>
-                </div>
-              );
-            })}
-          </List>
-        </Container>
-      </div>
-    )
-  }
-}
+export default Home;
